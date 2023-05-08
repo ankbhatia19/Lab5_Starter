15cee5c0640eccedb1fbe32ce905d6f48e838d8d
ankbhatia19 <ankbhatia19@gmail.com>
2023-05-07 21:17:56 -0700

    Added explore tts functionality

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -24,5 +24,33 @@ function populateVoiceList() {
 function init() {
     populateVoiceList();
 
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+        speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
 
+    // When the user clicks the "speak" button, speak the text in the textarea
+    document.querySelector('button').addEventListener('click', function() {
+        const text = document.getElementById("text-to-speak").value;
+        const utterance = new SpeechSynthesisUtterance(text);
+        const selectedOption = document.getElementById("voice-select").selectedOptions[0].getAttribute('data-name');
+        const voices = speechSynthesis.getVoices();
+
+        for (let i = 0; i < voices.length; i++) {
+            if (voices[i].name === selectedOption) {
+                utterance.voice = voices[i];
+            }
+        }
+
+        // Update the image to the open smile
+        const img = document.querySelector('img');
+        img.src = "assets/images/smiling-open.png";
+
+        // When the speech ends, change the image back to the closed smile
+        utterance.addEventListener('end', function() {
+            img.src = "assets/images/smiling.png";
+        });
+
+        speechSynthesis.speak(utterance);
+
+    });
 }
